afa60b0455e9df27c27c4a227bf827694c8a1817
wdmon33m <wdmon33m@gmail.com>
2023-09-04 00:52:34 -0500

    Head Dept Last Refactory

--- USTIT.Services.HeadDepartmentAPI/Program.cs
+++ USTIT.Services.HeadDepartmentAPI/Program.cs
@@ -1,11 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using USTIT.Services.HeadDepartmentAPI.Data;
+using USTIT.Services.HeadDepartmentAPI.Extentions;
+using USTIT.Services.HeadDepartmentAPI;
+using USTIT.Services.HeadDepartmentAPI.Repository.IRepository;
+using USTIT.Services.HeadDepartmentAPI.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
+});
+
+builder.Services.AddScoped<IAbsenceRepository, AbsenceRepository>();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(optionts =>
+{
+    optionts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "University of Science & Technology Version 1",
+        Description = "API to manage university system",
+        TermsOfService = new Uri("https://example.com/term"),
+        Contact = new OpenApiContact
+        {
+            Name = "Mohamed Ibrahem",
+            Email = "wdmon33m@gmail.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example license",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
+
+
+
+// Install the Microsoft.AspNetCore.Mvc.NewtonsoftJson via the NuGet package.Then, register the AddNewtonsoftJson()
+//builder.Services.AddControllers().AddNewtonsoftJson();
 
 var app = builder.Build();
 
@@ -13,7 +65,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "USTV1");
+    });
+}
+if (app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "USTV1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +85,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.ApplyMigration();
 app.Run();
